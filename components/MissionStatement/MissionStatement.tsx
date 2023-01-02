4b863b79a16eace55e48e8426b36e2cd540c3347
mdubelbeis <mason.dubelbeis@gmail.com>
2023-01-02 13:43:31 -0600

    created component/init layout

---
+++ components/MissionStatement/MissionStatement.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MissionStatement: React.FC = () => {
+  return (
+    <section className="mt-10 font-serif p-6 tracking-wide text-center text-md">
+      <h3>
+        Our mission at Du-Bee Herbs is to provide fresh and dried herbal
+        products for all to use in their daily lives. Naturally, grown and
+        produced on our Texas Homestead. Packaged, stored and shipped with
+        reusable, recycled, or compostable materials and containers
+      </h3>
+    </section>
+  );
+};
+
+export default MissionStatement;
